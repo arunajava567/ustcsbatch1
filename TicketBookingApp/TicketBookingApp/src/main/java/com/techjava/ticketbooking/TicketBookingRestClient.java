d38cfddd3bdcf33a1553517f44024fed4ad289ba
arunajava567 <30722352+arunajava567@users.noreply.github.com>
2021-11-22 22:17:19 +0530

    Add files via upload

---
+++ TicketBookingApp/TicketBookingApp/src/main/java/com/techjava/ticketbooking/TicketBookingRestClient.java
@@ -0,0 +1,46 @@
+package com.techjava.ticketbooking;
+
+import java.awt.PageAttributes.MediaType;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.techjava.ticketbooking.model.Ticket;
+@RestController
+public class TicketBookingRestClient {
+	
+
+	   @Autowired
+	   RestTemplate restTemplate;
+
+
+	public void getTicketDetails() {
+		
+	final String uri = "http://localhost:8085/booking/getTicketById/1";
+    RestTemplate restTemplate = new RestTemplate();
+    
+    
+    HttpHeaders headers = new HttpHeaders();
+   HttpEntity <String> entity = new HttpEntity<String>(headers);
+    
+     ResponseEntity<Ticket> response = restTemplate.getForEntity(uri, Ticket.class);
+     
+  System.out.println("Response from Servre:"+response.getBody().toString());
+  
+ //  System.out.println(restTemplate.exchange(uri, HttpMethod.GET, entity, String.class).getBody().toString());
+   // restTemplate.exchange("http://localhost:8085/booking/", HttpMethod.POST, entity, String.class).getBody();
+   //restTemplate.exchange("http://localhost:8085/products/"+id, HttpMethod.PUT, entity, String.class).getBody();
+   //restTemplate.exchange("http://localhost:8085/products/"+id, HttpMethod.DELETE, entity, String.class).getBody();
+   
+	}
+	public static void main(String[] args) {
+		TicketBookingRestClient client= new TicketBookingRestClient();
+		client.getTicketDetails();
+	}
+}
